087a43a6a0897fcee9ed0d143189dc426511efc0
Win <Worraphatwiriya9@gmail.com>
2026-07-20 13:54:00 +0700

    Update file:IndexPage.vue with in script

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -39,5 +39,36 @@
 </template>
 
 <script setup>
-//
+import { useQuasar } from 'quasar'
+import { ref } from 'vue'
+
+const $q = useQuasar()
+
+const name = ref(null)
+const age = ref(null)
+const accept = ref(false)
+
+function onSubmit() {
+  if (accept.value !== true) {
+    $q.notify({
+      color: 'red-5',
+      textColor: 'white',
+      icon: 'warning',
+      message: 'You need to accept the license and terms first'
+    })
+  } else {
+    $q.notify({
+      color: 'green-4',
+      textColor: 'white',
+      icon: 'cloud_done',
+      message: 'Submitted'
+    })
+  }
+}
+
+function onReset() {
+  name.value = null
+  age.value = null
+  accept.value = false
+}
 </script>
